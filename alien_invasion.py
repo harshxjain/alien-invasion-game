2cfc328af6f80eaa0f995222a1744d36d5949cc7
Harsh Jain <harshxjain6@gmail,com>
2023-04-25 22:23:38 +0530

    Refactored some code in the main file

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,17 +23,26 @@ class AlienInvasion:
         '''Start the main loop of the game'''
         
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            
+    def _check_events(self):
+        '''Respond to key presses and mouse movements'''
+        
+        # Check if the player has closed the window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Redraw the screen during each pass throught the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
-
+    def _update_screen(self):
+        '''Update images on screen and flip to the new screen'''
+        
+        # Redraw the screen during each pass throught the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+            
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game intance and run the game
